31aaf8248a55f64651dbc5ecf0db6804acbb862a
ankushmalode <you@example.com>
2021-10-16 18:26:18 +0530

    branch name update & indentation

--- src/main/java/com/bridgelabz/Stacks/Program.java
+++ src/main/java/com/bridgelabz/Stacks/Program.java
@@ -2,15 +2,15 @@ package com.bridgelabz.Stacks;
 
 public class Program {
     public static void main(String[] args) {
-        Stack stack=new Stack();
+        Stack stack = new Stack();
         stack.push(70);
         stack.push(30);
         stack.push(79);
-        System.out.println("Peek value in stack: "+stack.peak());
+        System.out.println("Peek value in stack: " + stack.peak());
         stack.push(56);
         stack.push(99);
         stack.show();
-        System.out.println("\nTop value pop: "+stack.pop());
+        System.out.println("\nTop value pop: " + stack.pop());
         stack.show();
     }
 }
--- src/main/java/com/bridgelabz/Stacks/Stack.java
+++ src/main/java/com/bridgelabz/Stacks/Stack.java
@@ -1,26 +1,30 @@
 package com.bridgelabz.Stacks;
 
 class Stack {
-    int[] stack=new int[5];
-    int top=0;
-    public void push(int data){
-        stack[top]=data;
+    int[] stack = new int[5];
+    int top = 0;
+
+    public void push(int data) {
+        stack[top] = data;
         top++;
     }
-    public void show(){
-        for (int i:stack)
-            System.out.print(i+" ");
+
+    public void show() {
+        for (int i : stack)
+            System.out.print(i + " ");
     }
-    public int pop(){
+
+    public int pop() {
         int data;
         top--;
-        data=stack[top];
-        stack[top]=0;
+        data = stack[top];
+        stack[top] = 0;
         return data;
     }
-    public int peak(){
+
+    public int peak() {
         int data;
-        data=stack[top-1];
+        data = stack[top - 1];
         return data;
     }
 }
