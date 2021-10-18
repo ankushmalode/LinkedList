38aa9fc327e904d460ddda1ddbae31d3d903b1bf
ankushmalode <you@example.com>
2021-10-18 16:37:01 +0530

    able to dequeue from beginning

--- src/main/java/com/bridgelabz/Queue/Program.java
+++ src/main/java/com/bridgelabz/Queue/Program.java
@@ -3,11 +3,19 @@ package com.bridgelabz.Queue;
 public class Program {
     public static void main(String[] args) {
         Queue queue=new Queue();
-
         queue.enQueue(56);
         queue.enQueue(30);
         queue.enQueue(70);
+        queue.show();
 
+        queue.deQueue();
+        queue.deQueue();
+        queue.enQueue(56);
+        queue.enQueue(30);
+        queue.show();
+
+        queue.deQueue();
+        queue.enQueue(70);
         queue.show();
     }
 }
--- src/main/java/com/bridgelabz/Queue/Queue.java
+++ src/main/java/com/bridgelabz/Queue/Queue.java
@@ -1,19 +1,26 @@
 package com.bridgelabz.Queue;
 
 public class Queue {
-    int[] queue=new int[5];
+    int[] queue=new int[4];
     int size,front,rear;
 
     public void enQueue(int data){
         queue[rear]=data;
-        rear++;
+        rear=(rear+1)%4;
         size++;
     }
 
     public void show(){
         System.out.print("Elements : ");
         for (int i=0;i<size;i++){
-            System.out.print(queue[i]+" ");
+            System.out.print(queue[(front+i)%4]+" ");
         }
+        System.out.println();
+    }
+    public int deQueue(){
+        int data=queue[front];
+        front++;
+        size--;
+        return data;
     }
 }
